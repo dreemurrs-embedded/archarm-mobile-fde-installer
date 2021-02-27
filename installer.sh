dreemurrs-embedded/archarm-mobile-fde-installer

commit 997b264eeace2f9b2b48a27953a247b75d8cfad0

Replace hardcoded cryptodevice name with mktemp output

installer.sh
@@ -155,7 +155,7 @@ PARTITIONS=$(lsblk $DISK_IMAGE -l | grep ' part ' | awk '{print $1}')
 BOOTPART=$(echo "$PARTITIONS" | sed -n '1p')
 ROOTPART=$(echo "$PARTITIONS" | sed -n '2p')
 
-ENCRYNAME="alarm_install"
+ENCRYNAME=$(basename $(mktemp -p /dev/mapper/ -u))
 ENCRYPART="/dev/mapper/$ENCRYNAME"
 
 echo "You'll now be asked to type in a new encryption key. DO NOT LOSE THIS!"

commit 05105b334075ecb75f3f6aba940ed6e5ff25339a

Fix boot and root partition path
Regression from 997b264eeace2f9b2b48a27953a247b75d8cfad0

installer.sh
@@ -152,8 +152,8 @@ sudo parted ${DISK_IMAGE} set 1 boot on --script
 
 # The first partition is the boot partition and the second one the root
 PARTITIONS=$(lsblk $DISK_IMAGE -l | grep ' part ' | awk '{print $1}')
-BOOTPART=$(echo "$PARTITIONS" | sed -n '1p')
-ROOTPART=$(echo "$PARTITIONS" | sed -n '2p')
+BOOTPART=/dev/$(echo "$PARTITIONS" | sed -n '1p')
+ROOTPART=/dev/$(echo "$PARTITIONS" | sed -n '2p')
 
 ENCRYNAME=$(basename $(mktemp -p /dev/mapper/ -u))
 ENCRYPART="/dev/mapper/$ENCRYNAME"